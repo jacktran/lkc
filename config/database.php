ee27d6ae17945e841d4db3afc8db828d24a98b95
unknown <phuc.tranhoang2012@gmail.com>
2015-06-13 12:59:26 +0700

    modified database

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', '23.94.223.135'),
-			'database'  => env('DB_DATABASE', 'admin_likecharm'),
-			'username'  => env('DB_USERNAME', 'admin_likecharm'),
-			'password'  => env('DB_PASSWORD', 'F5gG2A0iJm'),
+			'host'      => env('DB_HOST', ''),
+			'database'  => env('DB_DATABASE', ''),
+			'username'  => env('DB_USERNAME', ''),
+			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
